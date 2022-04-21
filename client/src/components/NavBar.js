ee7dd9a646683ff55e5e10d4d3546cf3ca0ba8df
Kjumar <jganguli@hotmail.ca>
2022-04-21 07:14:00 -0400

    changed app name in navbar (feel free to come up with a better one)

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -39,7 +39,7 @@ export default function NavBar() {
   return (
     <RBNavbar bg="dark" variant="dark" expand="md" fixed="top">
       <Container>
-        <RBNavbar.Brand>App Name</RBNavbar.Brand>
+        <RBNavbar.Brand>Medi-Connect</RBNavbar.Brand>
         <RBNavbar.Toggle aria-controls="navbar-collapsed" />
         <RBNavbar.Collapse id="navbar-collapsed">
           <Nav className="me-auto mb-2 mb-md-0">
